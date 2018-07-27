28ed9e5cbae3f588ae6ec07fce4f3a01183710ab
limuzhi <muzhi1991@gmail.com>
2018-07-27 08:59:11 +0800

    add commit for polymorphic

--- src/chapter2/PolymorphicByType.scala
+++ src/chapter2/PolymorphicByType.scala
@@ -5,10 +5,19 @@ package chapter2
   */
 object PolymorphicByType {
 
+  /**
+    * 部分应用函数，应用一部分已知的参数，如下面的f(a,b)只有a时，先应用a，函数编程了f(b)
+    * 类似于闭包
+    *
+    */
   def partial1[A, B, C](a: A, f: (A, B) => C): B => C = {
     (b: B) => f(a, b)
   }
 
+  /**
+    * 柯里化，对函数形态的改变
+    *
+    */
   def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
 
     (a: A) => {
@@ -17,6 +26,9 @@ object PolymorphicByType {
   }
 
 
+  /**
+    * 注意：由于=>是右结合的操作符，A => B => C 与 A => (B => C) 等价。含义是输入是一个值，输出是一个函数
+    */
   def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
     (a: A, b: B) => {
       f(a)(b)
@@ -24,6 +36,11 @@ object PolymorphicByType {
   }
 
 
+  /**
+    * 组合函数，从不同视角看
+    * * 改变了函数f的输入（最常用的视角），在Scala中 f.compose(g)就是这个功能
+    * * 改变了函数g的输出
+    */
   def compose[A, B, C](f: B => C, g: A => B): A => C = {
     (a: A) => {
       f(g(a))
